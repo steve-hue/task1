b3a309ef714ac1cc4a82a3fcb2367442d7aca2b7
Steve Stagy <stevesk2001@gmail.com>
2021-11-10 02:47:25 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react"
+
+import axios from "axios"
 
 function App() {
+
+  const URL="https://jsonplaceholder.typicode.com/todos"
+
+
+  const [create,setCreate]=useState([])
+  const [read,setRead]=useState([])
+  const [update,setUpdate]=useState([])
+  const [del,setDel]=useState([])
+
+
+  function createAPI(){
+    axios.post(URL,{
+      title:"API has been created"
+    }).then((response)=>{
+      console.log(response.data)
+      setCreate(response.data);
+    })
+
+  }
+
+  function readAPI(){
+    axios.get(`${URL}/1`).then((response)=>{
+      console.log(response.data)
+      setDel(response.data)
+      setUpdate(response.data)
+      setRead(response.data);
+    })
+  }
+
+  function updateAPI(){
+    axios.put(`${URL}/1`,{
+      title:"The API has been updated"
+    }).then((response)=>{
+      setUpdate(response.data)
+    })
+  }
+
+  function delAPI(){
+    axios.delete(`${URL}/1`).then((response)=>{
+      setDel([])
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <div>
+   <h1>Create and API</h1>
+   <p>{create.title}</p>
+   <button onClick={createAPI}>Create</button>
+
+   <h1>Read an API</h1>
+    <p>{read.title}</p>
+    <button onClick={readAPI}>Read</button>
+
+    <h1>Update an API</h1>
+    <p>{update.title}</p> 
+    <button onClick={updateAPI}>Update</button> 
+
+
+    <h1>Delete an API</h1>
+    <p>{del.title}</p>
+    <button onClick={delAPI}>Delete</button>
+
+
+  </div>
   );
 }
 
